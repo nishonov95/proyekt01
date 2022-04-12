70f195e3f6344c19b2cf1e48735ee1294c1afb79
Xolmurod <softrishton@gmail.com>
2022-04-12 16:59:00 +0500

    abjectlar bilan ishlandi

--- script/script.js
+++ script/script.js
@@ -1,71 +1,88 @@
 "use strict"
-let numberOfNews 
-function startProject() {
-    for (let j=1; j<2; j++){
-        numberOfNews = prompt("Siz qancha yangilik ko`rdingiz?")
-    
-        if (numberOfNews < 10 && numberOfNews != null && numberOfNews != ""){
-            alert("Siz yangilik ishqibozi emassiz ekan")
-            console.log("Siz yangilik ishqibozi emassiz ekan")
-        } else if (numberOfNews >= 10 && numberOfNews < 30 && numberOfNews != null && numberOfNews != ""){
-            alert("Siz klassik tomoshabinsz")
-            console.log("Siz klassik tomoshabinsz")
-        } else if(numberOfNews>=30 && numberOfNews != null && numberOfNews != "") {
-            alert("Siz yangiliklar ishqibozisiz")
-            console.log("Siz yangiliklar ishqibozisiz")
-        } else{
-            alert("siz harf kiritib qo`ydiz yoki hech narsa kiritmadiz qayta urinib ko`ring!!!")
-            console.log("siz harf kiritib qo`ydiz yoki hech narsa kiritmadiz qayta urinib ko`ring!!!")
-            j--
-        }
-    }    
-}
-
-startProject()
-
-
 
 const personalNewsDB = {
-    count: numberOfNews,
+    count: 0,
     news: {},
     actors: {},
     genres: [],
     privat: false,
-}
-
-function rememberNews() {
-    for (let i=1; i<3; i++){
-        let a = prompt(i+"- ko`rgan yangiligingiz qaysi?"),
-        b = prompt(i+"- yangilikga qancha baho bergan bo`lardingiz?")
-        if(a != null && b != null && a != "" && b != "" && a.length<50 && !isNaN(b)){
-            personalNewsDB.news[a] = b
-            alert(i+"- yangilik tayyor")
-            console.log(i+"- yangilik tayyor");
-        } else{
-            alert(i+"- yangilikni kiritmadiz yoki ball kiritadigan joyga harf ishlatdiz qaytadan "+i+"-savol beriladi")
-            console.log(i+"- yangilikni kiritmadiz yoki ball kiritadigan joyga harf ishlatdiz qaytadan "+i+"-savol beriladi");
-            i--
+    startProject: function(){
+        for (let j=1; j<2; j++){
+            personalNewsDB.count = prompt("Siz qancha yangilik ko`rdingiz?")
+        
+            if (personalNewsDB.count < 10 && personalNewsDB.count != null && personalNewsDB.count != ""){
+                alert("Siz yangilik ishqibozi emassiz ekan")
+                console.log("Siz yangilik ishqibozi emassiz ekan")
+            } else if (personalNewsDB.count >= 10 && personalNewsDB.count < 30 && personalNewsDB.count != null && personalNewsDB.count != ""){
+                alert("Siz klassik tomoshabinsz")
+                console.log("Siz klassik tomoshabinsz")
+            } else if(personalNewsDB.count>=30 && personalNewsDB.count != null && personalNewsDB.count != "") {
+                alert("Siz yangiliklar ishqibozisiz")
+                console.log("Siz yangiliklar ishqibozisiz")
+            } else{
+                alert("siz harf kiritib qo`ydiz yoki hech narsa kiritmadiz qayta urinib ko`ring!!!")
+                console.log("siz harf kiritib qo`ydiz yoki hech narsa kiritmadiz qayta urinib ko`ring!!!")
+                j--
+            }
+        } 
+    },
+    rememberNews: function(){
+        for (let i=1; i<3; i++){
+            let a = prompt(i+"- ko`rgan yangiligingiz qaysi?"),
+            b = prompt(i+"- yangilikga qancha baho bergan bo`lardingiz?")
+            if(a != null && b != null && a != "" && b != "" && a.length<50 && !isNaN(b)){
+                personalNewsDB.news[a] = b
+                alert(i+"- yangilik tayyor")
+                console.log(i+"- yangilik tayyor");
+            } else{
+                alert(i+"- yangilikni kiritmadiz yoki ball kiritadigan joyga harf ishlatdiz qaytadan "+i+"-savol beriladi")
+                console.log(i+"- yangilikni kiritmadiz yoki ball kiritadigan joyga harf ishlatdiz qaytadan "+i+"-savol beriladi");
+                i--
+            }
         }
-    }
-}
-rememberNews()
+    },
+    showMyDB: function(hidden){
+        if(!hidden){
+            console.log(personalNewsDB)
+        }
+    },
+    showAlert: function(){
+        alert("Hurmatli foydalanuvchi siz hozir 3 ta yoqadigan janr kiritishiz kerak")
+    },
+    toggleVisibleMyDB: function(){
+        if(personalNewsDB.privat = true){
+            personalNewsDB.privat = false
+        } else{
+            personalNewsDB.privat = true
+        }
+    },
+    writeYourGenres:function(){
+        for (let i = 1; i<4; i++){
+            let genre = prompt(`${i} -yoqadigan janriz`)
+                if(genre === "" || genre === null){
+                    alert(`siz ${i} chi janrni kiritishda xatoga yo'l qo'ydiz qayta urining`)
+                    console.log(`siz ${i} chi janrni kiritishda xatoga yo'l qo'ydiz qayta urining`)
+                    i--
+                } else {
+                    personalNewsDB.genres[i-1] = genre
+                    alert(`siz ${i} chi sevimli janrizni kiritib bo'ldiz`)
+                    console.log(`siz ${i} chi sevimli janrizni kiritib bo'ldiz`)
+                }
+        }
 
-function showMyDB(hidden){
-    if(!hidden){
-        console.log(personalNewsDB)
-    }
-
+    },
 }
-showMyDB(personalNewsDB.privat)
 
-function showAlert(){
-    alert("Hurmatli foydalanuvchi siz hozir 3 ta yoqadigan janr kiritishiz kerak")
-}
-showAlert()
+personalNewsDB.startProject()
+personalNewsDB.rememberNews()
+personalNewsDB.showAlert()
+personalNewsDB.writeYourGenres()
+personalNewsDB.showMyDB()
 
-function writeYourGenres(){
-    for (let i = 1; i<4; i++){
-        personalNewsDB.genres[i - 1]=prompt(`${i} -yoqadigan janriz`)
-    }
-}
-writeYourGenres()
+
+
+
+
+
+
+
